a2fc2b3fd5e9a4624950d598d9978e405f35e39f
Palash Debnath <palash@cefalo.com>
2021-08-15 00:09:03 +0600

    Initial commit

---
+++ samples/SampleAzureWebService/SampleKeyVaultClientWebRole/Constants.cs
@@ -0,0 +1,28 @@
+﻿// 
+// Copyright © Microsoft Corporation, All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+// ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+// PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+//
+// See the Apache License, Version 2.0 for the specific language
+// governing permissions and limitations under the License.
+
+namespace SampleKeyVaultClientWebRole
+{
+    internal static class Constants
+    {
+        public static string StorageAccountKeySecretUrlSetting = "nI1HaCOKasv1O9xr4uNIjw1bmemUOfU6wkGC50GIxh+wsxKsLEYYDz1kE9Z3F3pPn6+YUwHycHo6nf6KLp56Uw==";
+        public static string StorageAccountNameSetting = "vismadata";
+        public static string KeyVaultAuthClientIdSetting = "4c2b641c-a21c-4e95-a0ba-b149c1ddb5fa";
+        public static string KeyVaultAuthCertThumbprintSetting = "3GgpmCTSTJNXBYYIfKsPcU9yT3D3RUIx78s8XrwggNk=";
+        public static string KeyVaultSecretCacheDefaultTimeSpan = "KeyVaultSecretCacheDefaultTimeSpan";
+    }
+}
